6da6cc8ed3abdfb86e2b97e8649c404552b0bdcd
faithhub <adebayooluwadara@gmail.com>
2022-03-29 10:24:43 +0100

    updates

--- src/controllers/team.controllers.js
+++ src/controllers/team.controllers.js
@@ -1,12 +1,9 @@
-// import { createRequire } from "module";
-// const require = createRequire(import.meta.url);
 const ChatSupport= require("../db/models/support.models");
 const validate = require("../validations/support.validations");
 
 const text = async function (req, res, next) {
     res.status(200).json(
         {
-            // result: get,
             message: "test is okay now"
         }
     )

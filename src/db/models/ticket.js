0cce0fd96577bd449b6206264a1cd17e6b75cf48
faithhub <amaofaith.0@gmail.com>
2022-04-05 17:39:49 +0100

    update from associate

--- src/db/models/ticket.js
+++ src/db/models/ticket.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      // this.belongsTo(models.Room, {as: 'ChatRoom', foreignKey: 'ticketId'})
+      this.belongsTo(models.Room, {as: 'ChatRoom', foreignKey: 'ticketId'})
       this.hasOne(models.FeedBack, {as: 'FeedBack', foreignKey: 'ticketId'})
     }
   }

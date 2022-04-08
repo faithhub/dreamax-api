27ee1d290c72afad6699806d82d552f339b5deff
faithhub <amaofaith.o@gmail.com>
2022-04-08 15:16:10 +0100

    overall updates

--- src/controllers/team.controllers.js
+++ src/controllers/team.controllers.js
@@ -61,6 +61,174 @@ export default class {
     return { data: collateTeamMember };
   }
 
+  static async create(req) {
+    const { error } = validateSchema.validate(req.body);
+    if (error) {
+      return { error: error };
+    }
+
+    const { username } = req.body;
+
+    const checkUsername = await TeamMember.count({
+      where: { username },
+    });
+
+    if (checkUsername > 0) {
+      return { error: "The Team member username already exists" };
+    }
+
+    const createTeam = await TeamMember.create(req.body);
+    await TeamSetting.create({
+      adminId: createTeam.id,
+      fields: userSettings,
+    });
+
+    if (!createTeam) {
+      return { error: "An error occur when creating a new Team" };
+    }
+
+    return { data: createTeam };
+  }
+
+  static async get(req) {
+    const { id } = req.params;
+    const adminId = id;
+    const assignedTo = id;
+
+    const checkTeamMember = await TeamMember.findOne({
+      where: {
+        id,
+        deleted: 0,
+      },
+    });
+
+    if (!checkTeamMember) {
+      return { error: "No team member found for this id" };
+    }
+
+    const tickets = await Ticket.findAll({
+      where: {
+        id: id,
+        deleted: 0,
+      },
+    });
+
+    const closedTickets = tickets.filter(
+      (v) => v.assignedTo == adminId && v.status === "closed"
+    ).length;
+    const reolvedTickets = tickets.filter(
+      (v) => v.assignedTo == adminId && v.status === "resolved"
+    ).length;
+    const openTickets = tickets.filter(
+      (v) => v.assignedTo == adminId && v.status === "open"
+    ).length;
+    const unreolvedTickets = tickets.filter(
+      (v) => v.assignedTo == adminId && v.status === "unresolved"
+    ).length;
+    const assignedToTickets = tickets.length;
+
+    const teamMember = await TeamMember.findOne({
+      where: {
+        id,
+        deleted: 0,
+      },
+      include: [
+        {
+          model: Department,
+          attributes: ["id", "name", "status"],
+        },
+        {
+          model: TeamSetting,
+          attributes: ["id", "adminId", "fields"],
+        },
+        {
+          model: FeedBack,
+          attributes: ["id", "usercomment", "admincomment", "rating"],
+        },
+        {
+          model: Ticket,
+          attributes: [
+            "id",
+            "status",
+            "userId",
+            "assignedTo",
+            "piority",
+            "department",
+            "comment",
+            "ticketNo",
+          ],
+        },
+      ],
+    });
+
+    const feedBackTotalsum = await FeedBack.sum("rating", {
+      where: {
+        adminId: adminId,
+        deleted: 0,
+      },
+    });
+
+    const totalFeedbackCount = await FeedBack.count({
+      where: {
+        adminId: adminId,
+        deleted: 0,
+      },
+    });
+
+    const averageRating = feedBackTotalsum / totalFeedbackCount;
+
+    const sumAllTeamMemberTickets = await Ticket.findAll({
+      raw: true,
+      attributes: [
+        [
+          Sequelize.cast(
+            Sequelize.fn("SUM", Sequelize.col("respondTime")),
+            "float"
+          ),
+          "respondTime",
+        ],
+      ],
+      where: {
+        assignedTo: assignedTo,
+        deleted: 0,
+      },
+    });
+
+    const averageResponseTime = parseFloat(
+      sumAllTeamMemberTickets[0].respondTime / assignedToTickets
+    ).toFixed(2);
+
+    function convertTime(e) {
+      e = e * 60 || 0;
+      var h = Math.floor(e / 3600)
+          .toString()
+          .padStart(2, "0"),
+        m = Math.floor((e % 3600) / 60)
+          .toString()
+          .padStart(2, "0"),
+        s = Math.floor(e % 60)
+          .toString()
+          .padStart(2, "0");
+      return { hours: h, minutes: m, seconds: s };
+      //return `${h}:${m}:${s}`;
+    }
+
+    return {
+      data: {
+        averageRating,
+        averageResponseTime: convertTime(averageResponseTime),
+        summary: {
+          assignedToTickets,
+          openTickets,
+          closedTickets,
+          reolvedTickets,
+          unreolvedTickets,
+        },
+        teamMember,
+      },
+    };
+  }
+
   static async edit(req) {
     const { error } = editSchema.validate(req.body);
     if (error) {
